f42a8cb3d16a80b2ecdc5d09823714eae916d526
SarahMartin1244 <C00257967@setu.ie>
2023-10-07 13:00:34 +0100

    added barrier code to task function

--- labTwo/rendezvous.cpp
+++ labTwo/rendezvous.cpp
@@ -3,29 +3,46 @@
 #include <vector>
 #include <iostream>
 
+/*! Student Name: Sarah Martin */
+/*! Student Number: C00257967 */
+
 /*! displays the first function in the barrier being executed */
-void task(std::shared_ptr<Semaphore> mutexSem,std::shared_ptr<Semaphore> barrierSem, int threadCount){
+void task(std::shared_ptr<int> arrivedTasks, std::shared_ptr<Semaphore> mutexSem, std::shared_ptr<Semaphore> barrierSem, int threadCount){
 
   std::cout << "first " << std::endl;
   //put barrier code here
-  std::cout << "second" << std::endl;
+  mutexSem->Wait(); // ensure only one thread can update the shared 'arrivedTasks' variable at a time
+    *arrivedTasks = *arrivedTasks + 1; // Increment the number of arrived tasks
+
+   // If all threads have arrived at the barrier, signal the barrier semaphore.
+  if (*arrivedTasks == threadCount) {
+    barrierSem->Signal(); // Signal after all threads have arrived at the barrier
+  }
+  // Release the mutex semaphore to allow other threads to update 'arrivedTasks'.
+  mutexSem->Signal();
+  barrierSem->Wait();
+  barrierSem->Signal();
+  
+  std::cout << "second " << std::endl;
 }
 
 
 
 
 int main(void){
+  std::shared_ptr<int> arrivedTasks;
   std::shared_ptr<Semaphore> mutexSem;
   std::shared_ptr<Semaphore> barrierSem;
   int threadCount = 5;
   mutexSem=std::make_shared<Semaphore>(1);
   barrierSem=std::make_shared<Semaphore>(0);
+  arrivedTasks = std::make_shared<int>(0);
   /*!< An array of threads*/
   std::vector<std::thread> threadArray(threadCount);
   /*!< Pointer to barrier object*/
 
   for(int i=0; i < threadArray.size(); i++){
-    threadArray[i]=std::thread(task,mutexSem,barrierSem,threadCount);
+    threadArray[i]=std::thread(task,arrivedTasks,mutexSem,barrierSem,threadCount);
   }
 
   for(int i = 0; i < threadArray.size(); i++){
@@ -34,3 +51,4 @@ int main(void){
   
   return 0;
 }
+
